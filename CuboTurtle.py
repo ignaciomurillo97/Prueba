681ea4b02143890fa73ed9f5d30f9eff11559a09
F3nix123 <ignaciomurillo97@gmail.com>
2016-05-25 22:30:42 -0600

    modificaciones a cubo -nacho

--- CuboTurtle.py
+++ CuboTurtle.py
@@ -6,6 +6,11 @@ def elipse(angulo, radioMenor, altura):
     y = math.sin(angulo * 3.1415 / 180) * radioMenor + altura
     return (x, y)
 
+def circulo(angulo, radio):
+    x = math.cos(angulo * 3.1415 / 180) * radio
+    y = math.sin(angulo * 3.1415 / 180) * radio
+    return (x, y)
+
 def conectar(pt1, pt2):
     turtle.penup()
     turtle.goto(pt1)
@@ -35,13 +40,42 @@ def puntosCubo (lado, angulo):
     for i in range(len(pts2)):
         conectar(pts[i], pts2[i])
 
+def rombo(radio, alto, angulo):
+    pts = []
+    for i in range(6):
+        pts.append(elipse(angulo + 60 * i, radio, 0))
+
+    for i in range(len(pts)):
+        conectar(pts[i], pts[(i+1)%6])
+        conectar(pts[i], (0, alto))
+        conectar(pts[i], (0, -alto))
+
+def diamante(radio, alto, angulo):
+    pts = []
+    for i in range(6):
+        pts.append(elipse(angulo + 60 * i, radio, 0))
+
+    pts2 = []
+    for i in range(6):
+        pts2.append(elipse(angulo + 60 * i, radio * 0.7, alto/3))
+
+    for i in range(len(pts)):
+        conectar(pts[i], pts[(i+1)%6])
+        conectar(pts2[i], pts2[(i+1)%6])
+        conectar(pts[i], pts2[i])
+        conectar(pts[i], (0, -alto))
+
 i = 0
 turtle.ht()
+turtle.bgcolor(0.1, 0.1, 0.1)
+turtle.color(0.9, 0.9, 0.9)
 while True:
     turtle.tracer(0, i)
     puntosCubo(100, i)
-    puntosCubo(50, -i)
+    diamante(50, 100, i)
+    conectar((0, -250), (0, 250))
     turtle.update()
     turtle.clear()
     i+=0.25
+
 turtle.done()
